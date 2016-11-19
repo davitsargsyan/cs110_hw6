e2ea4347d11637a1ffb91a67f3e332eedfa925f3
Davit <davit_sargsyan@edu.aua.am>
2016-11-19 06:20:45 +0400

    first commit

---
+++ shapes/Rectangle.java
@@ -0,0 +1,15 @@
+package shapes;
+
+public class Rectangle {
+	private int startHeight;
+	private int startWidth;
+	
+	public Rectangle(int height, int width) {
+		this.startWidth = width;
+		this.startHeight = height;
+	}
+		
+	public int getArea(){
+		return startHeight * startWidth;
+	}
+}
